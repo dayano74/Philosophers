b4ee1d4b9560e6f983d39d0b639231ea8a42971f
dayano <kiixxn00@gmail.com>
2025-05-20 16:27:39 +0900

    update main func

--- src/main.c
+++ src/main.c
@@ -10,8 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "main.h"
+
+void	usage(char *program_name)
 {
-	printf("tmp\n");
+	printf("Usage: %s ", program_name);
+	printf("<number_of_philosophers> <time_to_die> ");
+	printf("<time_to_eat> <time_to_sleep> ");
+	printf("[optional: number_of_times_each_philosopher_must_eat]\n");
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 5 && argc != 6)
+		return (usage(argv[0]), 1);
 	return (0);
 }
