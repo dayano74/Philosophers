a79bd87db2e063e8e40f7af0b9bfca1aeab79782
dayano <kiixxn00@gmail.com>
2025-05-26 03:42:00 +0900

    update

--- inc/main.h
+++ inc/main.h
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:34:42 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/26 03:13:35 by dayano           ###   ########.fr       */
+/*   Updated: 2025/05/26 03:39:33 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,14 @@
 # include <sys/time.h>
 # include <string.h>
 # include "struct.h"
+# include "utils.h"
+# include "philo.h"
 
 // parse.c
-int		parse_args(int argc, char **argv);
-int		init_philosophers(void);
-int		create_threads(void);
-void	wait_for_threads(void);
-void	cleanup_resources(void);
-
-// utils.c
-int		ft_atoi(const char *str);
-int		is_valid_number(const char *str);
+int		parse_args(int argc, char **argv, t_data *data);
+int		create_threads(t_data *data);
+void	wait_for_threads(t_data *data);
+void	cleanup_resources(t_data *data);
 
 long	get_timestamp(void);
 void	print_status(t_philo *philo, char *status);
