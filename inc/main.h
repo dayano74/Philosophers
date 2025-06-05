88dc1c91a7335457a71b8bb0faffc2734a50d8b2
dayano74 <kiixxn00@gmail.com>
2025-06-05 15:54:38 +0900

    update

--- inc/main.h
+++ inc/main.h
@@ -6,7 +6,7 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:34:42 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/27 21:38:59 by dayano           ###   ########.fr       */
+/*   Updated: 2025/06/05 15:53:07 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,23 @@
 
 // parse.c
 int		parse_args(int argc, char **argv, t_data *data);
+
+// philo.c
+int		init_mutex_and_philo(t_data *data);
+
+// thread.c
 int		create_threads(t_data *data);
 void	wait_for_threads(t_data *data);
-void	cleanup_resources(t_data *data);
 
+// actions.c
+void	eat(t_philo *philo);
+void	sleep(t_philo *philo);
+void	think(t_philo *philo);
+
+// utils.c
 long	get_timestamp(void);
 void	print_status(t_philo *philo, char *status);
+
+void	cleanup_resources(t_data *data);
 
 #endif
