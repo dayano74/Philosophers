07085d764291a1a0f518d04f7e438da748268822
dayano74 <kiixxn00@gmail.com>
2025-06-05 15:45:39 +0900

    update

--- src/philo.c
+++ src/philo.c
@@ -6,11 +6,31 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 03:35:56 by dayano            #+#    #+#             */
-/*   Updated: 2025/05/27 22:02:08 by dayano           ###   ########.fr       */
+/*   Updated: 2025/06/05 15:43:25 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+static void	cleanup_mutexes(t_data *data)
+{
+	int i;
+
+	if (data->forks)
+	{
+		i = 0;
+		while(i < data->philo_count)
+		{
+			pthread_mutex_destroy(&data->forks[i]);
+			i++;
+		}
+		free(data->forks);
+		data->forks = NULL;
+	}
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->death_mutex);
+
+}
 
 static int	init_mutexes(t_data *data)
 {
@@ -40,6 +60,29 @@ static int	init_mutexes(t_data *data)
 	return (0);
 }
 
+static int	init_philos(t_data *data)
+{
+	int i;
+
+	data->philos = malloc(sizeof(t_philo) * data->philo_count);
+	if (!data->philos)
+		return (1);
+	i = 0;
+	while(i < data->philo_count)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].meals_eaten = 0;
+		data->philos[i].last_meal_time = 0;
+		data->philos[i].data = data;
+		data->philos[i].left_fork = &data->forks[i];
+		data->philos[i].right_fork = &data->forks[(i + 1) % data->philo_count];
+		i++;
+	}
+	data->start_time = 0;
+	data->is_simulation_end = 0;
+	return (0);
+}
+
 int		init_mutex_and_philo(t_data *data)
 {
 	if (init_mutexes(data) != 0)
@@ -49,4 +92,5 @@ int		init_mutex_and_philo(t_data *data)
 		cleanup_mutexes(data);
 		return (1);
 	}
+	return (0);
 }
